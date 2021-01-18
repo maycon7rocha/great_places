ec203d9303928ff2e2eb37843921e4cf30fa798f
Maycon Rocha <maycon7rocha@gmail.com>
2021-01-18 16:43:46 -0300

    329

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:great_places/providers/great_places.dart';
 import 'package:great_places/screens/place_form_screen.dart';
-import 'package:great_places/screens/places_list.dart';
+import 'package:great_places/screens/places_list_screen.dart';
 import 'package:great_places/utils/app_routes.dart';
 import 'package:provider/provider.dart';
 
